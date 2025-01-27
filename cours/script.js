584553af20408f277db561eb459229967cbf85b5
Loicsoh <www.sohloic132@gmail.com>
2025-01-27 15:10:20 +0100

    test de donnees

---
+++ cours/script.js
@@ -0,0 +1,10 @@
+let string = "Hello World";
+console.log(typeof(string)); // string
+let number = 10;
+console.log(typeof(number)); // number
+let boolean = true;
+console.log(typeof(boolean)); // boolean
+let array = [1, 2, 3];
+console.log(typeof(array)); // object
+let object = {name: "John", age: 25};
+console.log(typeof(object)); // object

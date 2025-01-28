221129a40485036c2cdfa31ed91bb4abbf8702f2
Loicsoh <www.sohloic132@gmail.com>
2025-01-28 13:47:34 +0100

    text typing

--- text_typin/script.js
+++ text_typin/script.js
@@ -5,4 +5,36 @@ let array =["Developpeur","Graphist","Web-design"];
 let wordIndex = 0;
 
 let letterIndex = 0;
-target.textContent = array[2],[0];
+
+const createLetter = () => {
+    const letter = document.createElement('span');
+    target.appendChild(letter);
+    letter.textContent = array[wordIndex][letterIndex];
+    setTimeout(() => {
+        letter.remove()
+        }, 2000)
+
+}
+
+const loop = () => {
+    setTimeout(() => {
+        if (wordIndex >= array.length) {
+            wordIndex = 0;
+            letterIndex = 0;
+            loop();
+            return
+        } 
+        if (letterIndex < array[wordIndex].length) {
+            createLetter();
+            letterIndex++;
+            loop();
+            }else{
+                setTimeout(() => {
+                wordIndex++;
+                letterIndex = 0;
+                loop();
+            }, 2000);
+        }
+    }, 60)
+};
+loop()

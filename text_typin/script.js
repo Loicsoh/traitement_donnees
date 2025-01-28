389ffee665f94368e762d794a39cfa53208003b1
Loicsoh <www.sohloic132@gmail.com>
2025-01-28 10:21:45 +0100

    text-typing

---
+++ text_typin/script.js
@@ -0,0 +1,8 @@
+let target = document.getElementById('target');
+
+let array =["Developpeur","Graphist","Web-design"];
+
+let wordIndex = 0;
+
+let letterIndex = 0;
+target.textContent = array[2],[0];
